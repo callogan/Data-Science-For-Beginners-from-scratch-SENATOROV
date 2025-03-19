d300c6c235ca49c27397e52582d5aa38fd90e282
Ruslan Kazmiryk <callogan217@gmail.com>
2025-03-19 21:01:41 +0200

    fix(rounding): fix the logic of number rounding

--- Python/round_numbers.py
+++ Python/round_numbers.py
@@ -3,4 +3,5 @@
 import random
 
 number_1 = random.random()  # генерирование числа от 0 до 1
-print(round(number_1, 2))  # вывод на печать
+number_1 = round(number_1, 2)  # округление значения сразу в переменной
+print(number_1)  # вывод на печать уже округленного числа
