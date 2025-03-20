6c53f0c35d47a70c4dfd8a15b9d92451ae6a0360
Ruslan Kazmiryk <callogan217@gmail.com>
2025-03-20 11:55:34 +0200

    style: correct code formatting

--- Python/round_numbers.py
+++ Python/round_numbers.py
@@ -2,10 +2,14 @@
 
 import random
 
+# +
 number_1 = random.random()  # генерирование числа от 0 до 1
 number_1 = round(number_1, 2)  # округление значения сразу в переменной
+
 print(number_1)  # вывод на печать уже округленного числа
 
+
+# -
 
 def generate_report(rounded_number: float) -> None:
     """Формирует отчет с использованием числа."""
