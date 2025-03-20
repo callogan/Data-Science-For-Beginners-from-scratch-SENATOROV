6496ed0f92afb364253186846433bdc029eb5cdf
Ruslan Kazmiryk <callogan217@gmail.com>
2025-03-20 11:39:48 +0200

    feat(reporting): add generate_report function

--- Python/round_numbers.py
+++ Python/round_numbers.py
@@ -5,3 +5,9 @@ import random
 number_1 = random.random()  # генерирование числа от 0 до 1
 number_1 = round(number_1, 2)  # округление значения сразу в переменной
 print(number_1)  # вывод на печать уже округленного числа
+
+
+def generate_report(rounded_number: float) -> None:
+    """Формирует отчет с использованием числа."""
+    report = f"Отчет о вычислениях (округлено): {rounded_number}"
+    print(report)
