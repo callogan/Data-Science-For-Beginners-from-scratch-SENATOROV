5d2ecdc88362887db16a4b64428b7f7a631421a5
Ruslan Kazmiryk <callogan217@gmail.com>
2025-04-20 10:23:53 +0300

    Create test_dummy.py

---
+++ tests/test_dummy.py
@@ -0,0 +1,7 @@
+"""This module contains a placeholder test to satisfy pytest requirements."""
+
+
+def test_dummy() -> None:
+    """Placeholder test to prevent pytest from reporting zero collected
+    tests."""
+    return None
