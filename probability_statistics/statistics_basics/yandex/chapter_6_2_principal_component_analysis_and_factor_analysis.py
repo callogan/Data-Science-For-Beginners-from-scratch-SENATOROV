2bdc24c66a5b9dbb2279db285cdbce5681fa737a
Ruslan Kazmiryk <callogan217@gmail.com>
2025-09-20 10:06:32 +0300

    Yandex tasks [SENATOROVAI#7] (https://github.com/SENATOROVAI/DA/issues/7)
    Closes https://github.com/SENATOROVAI/DA/issues/7

---
+++ probability_statistics/statistics_basics/yandex/chapter_6_2_principal_component_analysis_and_factor_analysis.py
@@ -0,0 +1,38 @@
+"""Principal component analysis and factor analysis."""
+
+# ## Метод главных компонент и факторный анализ
+
+# +
+# 1
+
+import pandas as pd
+
+
+world_wave = pd.read_csv("WV6_Data_csv_v20201117.csv", sep=";")
+
+print(world_wave.dtypes)
+
+
+# +
+# 2
+
+print(4)
+
+# +
+# 3
+
+
+print("V119", "V120", "V121", "V122", "V123")
+
+
+# +
+# 4
+
+
+print("V108", "V109", "V117", "V118")
+
+# +
+# 5
+
+
+print("V117")
